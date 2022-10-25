b88468be98d1816873815b05639c7a89ff0e8236
Santino Stimoli <107507086+santino-stimoli@users.noreply.github.com>
2022-10-25 17:33:35 -0300

    Starting home

---
+++ src/Components/pure/Carousel.jsx
@@ -0,0 +1,34 @@
+import { Carousel } from 'flowbite-react';
+import React from 'react';
+import Image1 from '../../resources/image1.jpg'
+import Image2 from '../../resources/image2.jpg'
+import Image3 from '../../resources/image3.jpg'
+
+const CarouselHome = () => {
+    return (
+        <div className="h-64 sm:h-80 xl:h-96 2xl:h-96">
+            <Carousel slideInterval={5000}
+                leftControl=" "
+                rightControl=" "
+            >
+                <img
+                    className='lg:pb-32'
+                    src={Image1}
+                    alt="..."
+                />
+                <img
+                    className='lg:pb-32'
+                    src={Image2}
+                    alt="..."
+                />
+                <img
+                    className='lg:pb-30'
+                    src={Image3}
+                    alt="..."
+                />
+            </Carousel>
+        </div>
+    );
+}
+
+export default CarouselHome;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import CarouselHome from '../Components/pure/Carousel';
 import Title from '../Components/pure/Title';
 
 const Home = () => {
     return (
-        <div className='container mx-auto px-2 py-2.5 sm:px-4'>
-            <Title title={'Home'}>
-                <h2>Welcome to the best events page.</h2>
-                <p>AMAZING EVENTS is a page where you will be able to see the information of the best events that have been and will be, you will also be able to get your ticket to participate in them with your preferred debit or credit card under our 100% secure encrypted payments</p>
-            </Title>
+        <div className='container mx-auto px-2 sm:px-4 mt-5'>
+            <CarouselHome />
+            <div className='my-10'>
+                <Title>
+                    <h2>Welcome to the best events page.</h2>
+                    <p>AMAZING EVENTS is a page where you will be able to see the information of the best events that have been and will be, you will also be able to get your ticket to participate in them with your preferred debit or credit card under our 100% secure encrypted payments</p>
+                </Title>
+            </div>
         </div>
     );
 }
