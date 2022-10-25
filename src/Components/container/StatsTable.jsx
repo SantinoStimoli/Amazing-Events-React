f4f567d593419b9a341fdcbfa9989506a16b6f5c
Santino Stimoli <107507086+santino-stimoli@users.noreply.github.com>
2022-10-25 00:01:59 -0300

    function stats part 1

--- src/Components/container/StatsTable.jsx
+++ src/Components/container/StatsTable.jsx
@@ -5,18 +5,15 @@ import Table from '../pure/Table';
 
 const StatsTable = () => {
 
-    const [maxRevenue, setMaxRevenue] = useState(0);
-    const [categoryMaxRevenue, setCategoryMaxRevenue] = useState(0);
-    const [totalRevenue, setTotalRevenue] = useState(0);
-
-    const [maxAssistance, setMaxAssistance] = useState(0);
-    const [categoryMaxAssistance, setCategoryMaxAssistance] = useState(0);
-    const [totalAssistance, setTotalAssistance] = useState(0);
-
-    const [categories, setCategories] = useState([]);
-    const [categoriesStats, setCategoriesStats] = useState([]);
-
     const [events, setEvents] = useState([]);
+    const [categories, setCategories] = useState([]);
+    // const [food, setFood] = useState([]);
+    // const [museum, setMuseum] = useState([]);
+    // const [party, setParty] = useState([]);
+    // const [music, setMusic] = useState([]);
+    // const [race, setRace] = useState([]);
+    // const [book, setBook] = useState([]);
+    // const [cinema, setCinema] = useState([]);
 
     useEffect(() => {
         obtainEvents().then((response) => {
@@ -25,11 +22,97 @@ const StatsTable = () => {
         obtainCategories(setCategories)
     }, []);
 
-    console.log(categories, events);
+
+    function obtainMaxAssistance() {
+        let total = 0;
+        let categoryOfTotal = '';
+        let innerText = ''
+
+        categories.forEach(category => {
+            let filtrado = events.filter(event => event.category === category)
+            let totalToCompare = 0;
+
+            filtrado.forEach(event => {
+                totalToCompare = totalToCompare + parseInt(event.assistance ? event.assistance : event.estimate)
+                return totalToCompare
+            })
+
+            if (total < totalToCompare) {
+                total = totalToCompare
+                categoryOfTotal = category
+            }
+            innerText = categoryOfTotal + ': ' + total
+            return innerText
+        })
+
+        return innerText
+    }
+
+    function obtainTotalAssistance() {
+        let allAssistance = 0;
+
+        categories.forEach(category => {
+            let filtrado = events.filter(event => event.category === category)
+            let totalAssistance = 0;
+
+            filtrado.forEach(event => {
+                totalAssistance = totalAssistance + parseInt(event.assistance ? event.assistance : event.estimate)
+                return totalAssistance
+            })
+            allAssistance = allAssistance + totalAssistance
+
+            return allAssistance
+        })
+
+        return allAssistance
+    }
+
+    function obtainMaxRevenue() {
+        let total = 0;
+        let categoryOfTotal = '';
+        let innerText = ''
+
+        categories.forEach(category => {
+            let filtrado = events.filter(event => event.category === category)
+            let totalToCompare = 0;
+            filtrado.forEach(event => {
+                totalToCompare = totalToCompare + parseInt((event.assistance ? event.assistance : event.estimate) * event.price)
+                return totalToCompare
+            })
+
+            if (total < totalToCompare) {
+                total = totalToCompare
+                categoryOfTotal = category
+            }
+            innerText = categoryOfTotal + ': ' + total
+            return innerText
+        })
+        return innerText
+    }
+
+    function obtainTotalRevenue() {
+        let allRevenue = 0;
+
+        categories.forEach(category => {
+            let filtrado = events.filter(event => event.category === category)
+            let totalRevenue = 0;
+
+            filtrado.forEach(event => {
+                totalRevenue = totalRevenue + parseInt((event.assistance ? event.assistance : event.estimate) * event.price)
+                return totalRevenue
+            })
+            allRevenue = allRevenue + totalRevenue
+
+            return allRevenue
+        })
+
+        return allRevenue
+    }
+
 
     return (
         <div>
-            <Table />
+            <Table maxAssistance={obtainMaxAssistance()} maxRevenue={obtainMaxRevenue()} totalAssistance={obtainTotalAssistance()} totalRevenue={obtainTotalRevenue()} />
         </div>
     );
 }
