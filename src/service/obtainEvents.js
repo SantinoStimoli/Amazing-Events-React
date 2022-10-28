d5b8801e6e03bdc9ecebd01ef02d17785249e57c
Santino Stimoli <107507086+santino-stimoli@users.noreply.github.com>
2022-10-28 11:54:44 -0300

    axios

--- src/service/obtainEvents.js
+++ src/service/obtainEvents.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import { Axios } from "axios"
 
 const URL = 'https://amazing-events.herokuapp.com/api/events'
 
 export function obtainEvents() {
-    return axios.get(URL)
+    return Axios.get(URL)
 }
